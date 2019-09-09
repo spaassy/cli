41a73e5635ced8c3e29ee68f31920ef470e5625f
getYourElementById <zhyf0310@163.com>
2019-09-09 22:00:19 +0800

    提交

--- cli/bin/index.js
+++ cli/bin/index.js
@@ -10,8 +10,11 @@ const download = require('download-git-repo');
 const chalk = require('chalk')
 const utils = require('../utils')
 const path = require('path')
+const inquirer = require('inquirer')
 
 const fileIsExist = utils.fileIsExist
+const readFileSync = utils.readFileSync
+const writeFileSync = utils.writeFileSync
 
 const gitUrl = 'github:spaassy/template'
 
@@ -21,25 +24,73 @@ program
     .option("-p --project [projectName]", "set project name default 'spaassy'", 'spaassy')
     .alias('i')
     .action(function (cmd) {
-        const spinner = ora('正在初始化项目……').start();
         const projectName = cmd.project
-        // require('./init')(projectName)
         const indexPath = process.cwd()
-        if(fileIsExist(path.resolve(indexPath, `${projectName}`))){
-            console.info(chalk.red(`\n${projectName} 文件夹已存在，请先删除！`));
-            spinner.stop()
-            return
-        }
+        console.log(chalk.blue('填写项目相关信息，你也可以直接使用默认值，然后在package.json文件中修改！'))
 
-        download(`${gitUrl}#master`, projectName, function (err) {
-            spinner.stop()
-            if (!err) {
-                // 可以输出一些项目成功的信息
-                console.info(chalk.blueBright('模板初始化成功！'));
-            } else {
-                // 可以输出一些项目失败的信息
-                console.log(chalk.red(err))
+        inquirer.prompt([{
+                type: String,
+                name: 'name',
+                message: '项目名称:',
+                default: projectName
+            },
+            {
+                type: String,
+                name: 'version',
+                message: '版本:',
+                default: '1.0.0'
+            },
+            {
+                type: String,
+                name: 'description',
+                message: '描述:',
+                default: ''
+            },
+            {
+                type: String,
+                name: 'author',
+                message: '作者:',
+                default: ''
+            },
+            {
+                type: String,
+                name: 'license',
+                message: '许可:',
+                default: 'ISC'
             }
+        ]).then(answers => {
+            const spinner = ora('正在初始化项目……').start();
+            console.log(answers)
+
+            if (fileIsExist(path.resolve(indexPath, `${projectName}`))) {
+                console.info(chalk.red(`\n${projectName} 文件夹已存在，请先删除！`));
+                spinner.stop()
+                return
+            }
+
+            download(`${gitUrl}#master`, projectName, function (err) {
+                spinner.stop()
+                if (!err) {
+                    let tempPackageData = null
+                    if (fileIsExist(path.resolve(indexPath, `${projectName}/package.json`))) {
+                        let tempPackageJsonPath = path.resolve(indexPath, `${projectName}/package.json`)
+                        tempPackageData = readFileSync(tempPackageJsonPath)
+                        let tempPackageJson = JSON.parse(tempPackageData)
+
+                        Object.keys(answers).map(key => {
+                            tempPackageJson[key] = answers[key]
+                        })
+
+                        let newTempPackageJson = JSON.stringify(tempPackageJson, null, '\t')
+                        writeFileSync(tempPackageJsonPath, newTempPackageJson)
+                    }
+                    // 可以输出一些项目成功的信息
+                    console.info(chalk.blueBright('模板初始化成功！'));
+                } else {
+                    // 可以输出一些项目失败的信息
+                    console.log(chalk.red(err))
+                }
+            })
         })
     })
 
